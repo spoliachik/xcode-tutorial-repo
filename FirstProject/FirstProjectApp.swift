eff7a4ff2ba3ef53a0ceccae8304e081520b6052
Shelby Poliachik <spoliachik@captechconsulting.com>
2021-10-27 14:02:42 -0400

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Shelby Poliachik on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
